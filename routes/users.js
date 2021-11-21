6d2c12b251abcac792550d186881e8d6f0a578f4
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-11-21 15:34:54 +0600

    Get latest data in all users api

--- routes/users.js
+++ routes/users.js
@@ -62,7 +62,9 @@ router.get("/", verify, async (req, res) => {
   const query = req.query.new;
   if (req.user.isAdmin) {
     try {
-      const users = query ? await User.find().limit(10) : await User.find();
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(10)
+        : await User.find();
       res.status(200).json(users);
     } catch (err) {
       res.status(500).json(err);
