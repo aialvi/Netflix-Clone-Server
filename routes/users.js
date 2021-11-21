65dacd4320d8479900f7066cc24ef0463da19f04
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-11-21 17:25:50 +0600

    Add user stats

--- routes/users.js
+++ routes/users.js
@@ -74,4 +74,26 @@ router.get("/", verify, async (req, res) => {
   }
 });
 
+//GET USER STATS
+router.get("/stats", async (req, res) => {
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
