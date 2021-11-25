91bc4be28d22dc286462613ec5a4e4c8eb2615ec
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-11-25 16:49:42 +0600

    Modify movie routes

--- routes/movies.js
+++ routes/movies.js
@@ -56,7 +56,7 @@ router.delete("/:id", verify, async (req, res) => {
 
 //GET
 
-router.get("/find/:id", verify, async (req, res) => {
+router.get("find/:id", verify, async (req, res) => {
   try {
     const movie = await Movie.findById(req.params.id);
     res.status(200).json(movie);
